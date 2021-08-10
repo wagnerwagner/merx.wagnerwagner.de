159e9edc17e69a8638857fe6f76e131b78f2e163
Tobias Wolf <tobias.wolf@wagnerwagner.de>
2021-08-10 15:50:36 +0200

    Improves hero.scss

--- develop/gulpfile.js
+++ develop/gulpfile.js
@@ -1,5 +1,5 @@
 const gulp = require('gulp');
-const sass = require('gulp-sass');
+const sass = require('gulp-dart-sass');
 const autoprefixer = require('gulp-autoprefixer');
 const sourcemaps = require('gulp-sourcemaps');
 const imagemin = require('gulp-imagemin');
