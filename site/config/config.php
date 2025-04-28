c5d042ac76e909dde8c7b5e2233cf1fbc4c74f61
Tobias Wolf <tobias.wolf@wagnerwagner.de>
2025-04-28 17:18:44 +0200

    Switch to config/env.php

--- site/config/config.php
+++ site/config/config.php
@@ -1,19 +1,4 @@
 <?php
-$stripeTestPublishableKey = file_get_contents('secrets/.stripeTestPublishableKey', FILE_USE_INCLUDE_PATH);
-$stripeTestSecretKey = file_get_contents('secrets/.stripeTestSecretKey', FILE_USE_INCLUDE_PATH);
-$paypalSandboxClientID = file_get_contents('secrets/.paypalSandboxClientID', FILE_USE_INCLUDE_PATH);
-$paypalSandboxSecret = file_get_contents('secrets/.paypalSandboxSecret', FILE_USE_INCLUDE_PATH);
-
-$stripeLivePublishableKey = file_get_contents('secrets/.stripeLivePublishableKey', FILE_USE_INCLUDE_PATH);
-$stripeLiveSecretKey = file_get_contents('secrets/.stripeLiveSecretKey', FILE_USE_INCLUDE_PATH);
-$paypalLiveClientID = file_get_contents('secrets/.paypalLiveClientID', FILE_USE_INCLUDE_PATH);
-$paypalLiveSecret = file_get_contents('secrets/.paypalLiveSecret', FILE_USE_INCLUDE_PATH);
-
-$smtpHost = file_get_contents('secrets/.smtpHost', FILE_USE_INCLUDE_PATH);
-$smtpUsername = file_get_contents('secrets/.smtpUsername', FILE_USE_INCLUDE_PATH);
-$smtpPassword = file_get_contents('secrets/.smtpPassword', FILE_USE_INCLUDE_PATH);
-
-$merxLicense = file_get_contents('secrets/.merxLicense', FILE_USE_INCLUDE_PATH);
 
 return [
   'github-repository' => 'https://github.com/wagnerwagner/merx',
@@ -21,24 +6,4 @@ return [
   'merxPluginDirectory' => '/site/plugins/merx',
   'merx-email' => 'merx@wagnerwagner.de',
   'ww.merx.production' => true,
-  'ww.merx.license' => $merxLicense,
-  'ww.merx.stripe.test.publishable_key' => $stripeTestPublishableKey,
-  'ww.merx.stripe.test.secret_key' => $stripeTestSecretKey,
-  'ww.merx.stripe.live.publishable_key' => $stripeLivePublishableKey,
-  'ww.merx.stripe.live.secret_key' => $stripeLiveSecretKey,
-  'ww.merx.paypal.sandbox.clientID' => $paypalSandboxClientID,
-  'ww.merx.paypal.sandbox.secret' => $paypalSandboxSecret,
-  'ww.merx.paypal.live.clientID' => $paypalLiveClientID,
-  'ww.merx.paypal.live.secret' => $paypalLiveSecret,
-  'email' => [
-    'transport' => [
-      'type' => 'smtp',
-      'host' => $smtpHost,
-      'port' => 465,
-      'auth' => true,
-      'security' => true,
-      'username' => $smtpUsername,
-      'password' => $smtpPassword,
-    ],
-  ],
 ];
